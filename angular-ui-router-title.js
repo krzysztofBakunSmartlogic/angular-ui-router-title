710a434b55fca44ac1ef5143f5131f193b2086f5
Krzysztof Bakun <krzysztof.bakun@smartlogic.com>
2016-08-30 12:03:50 +0200

    Builded.

--- angular-ui-router-title.js
+++ angular-ui-router-title.js
@@ -10,35 +10,40 @@
 
 (function(angular) {
 
-"use strict";
-angular.module("ui.router.title", ["ui.router"])
-	.run(["$rootScope", "$timeout", "$state", function($rootScope, $timeout, $state) {
+(function () {
+  "use strict";
 
-		$rootScope.$on("$stateChangeSuccess", function() {
-			var title = getTitleValue($state.$current.locals.globals.$title);
-			$timeout(function() {
-				$rootScope.$title = title;
-			});
+  angular.module("ui.router.title", ["ui.router"])
+    .run(routerTitle);
+  routerTitle.$inject = ["$rootScope", "$timeout", "$state"];
+  function routerTitle($rootScope, $timeout, $state) {
 
-			$rootScope.$breadcrumbs = [];
-			var state = $state.$current;
-			while(state) {
-				if(state.resolve && state.resolve.$title) {
-					$rootScope.$breadcrumbs.unshift({
-						title: getTitleValue(state.locals.globals.$title),
-						state: state.self.name,
-						stateParams: state.locals.globals.$stateParams
-					})
-				}
-				state = state.parent;
-			}
-		});
+    $rootScope.$on("$stateChangeSuccess", function () {
+      var title = getTitleValue($state.$current.locals.globals.$title);
+      $timeout(function () {
+        $rootScope.$title = title;
+      });
 
-		function getTitleValue(title) {
-			return angular.isFunction(title) ? title() : title;
-		}
+      $rootScope.$breadcrumbs = [];
+      var state = $state.$current;
+      while (state) {
+        if (state.resolve && state.resolve.$title) {
+          $rootScope.$breadcrumbs.unshift({
+            title: getTitleValue(state.locals.globals.$title),
+            state: state.self.name,
+            stateParams: state.locals.globals.$stateParams
+          })
+        }
+        state = state.parent;
+      }
+    });
 
-	}]);
+    function getTitleValue(title) {
+      return angular.isFunction(title) ? title() : title;
+    }
 
+  }
+
+})();
 
 })(window.angular);
